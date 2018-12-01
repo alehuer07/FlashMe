d2b8bd2d0e07cda8fae30684bd2f793fb93b40f1
Alexis Huerta <alehuer90@gmail.com>
2018-12-01 13:29:48 -0800

    Finished adding in the database. Could use polishing

--- app/src/main/java/com/example/alehu/flashme/MainActivity.java
+++ app/src/main/java/com/example/alehu/flashme/MainActivity.java
@@ -6,12 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        //if list is not empty, display a saved flashcard.
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
 
 
         findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
@@ -65,21 +76,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
-            String string1 = data.getExtras().getString("newQuestion"); // 'string1' needs to match the key we used when we put the string in the Intent
-            String string2 = data.getExtras().getString("newAnswer");
+            String question = data.getExtras().getString("newQuestion"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String answer = data.getExtras().getString("newAnswer");
 
             //Set text
             TextView newQuestionCard = findViewById(R.id.flashcard_question);
-            newQuestionCard.setText(string1);
+            newQuestionCard.setText(question);
             TextView newAnswerCard = findViewById(R.id.flashcard_answer);
-            newAnswerCard.setText(string2);
+            newAnswerCard.setText(answer);
+
+            //insert new card into database
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
+
+    List<Flashcard> allFlashcards;
+    FlashcardDatabase flashcardDatabase;
+
+    int currentCardDisplayedIndex = 0;
+
+
 
 }
